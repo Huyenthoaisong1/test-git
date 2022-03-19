24861e95bbc5846496a6a7607d11abf6b9ec0329
Huyenthoaisong1 <whydoihavetousefacebook@gmail.com>
2022-03-19 23:01:31 +0700

    3/18/2022 test git gub

---
+++ src/java/model/event.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author sioni
+ */
+public class event {
+    int id;
+    String content;
+    Date date;
+    boolean isEnabled;
+    String Created_by;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isIsEnabled() {
+        return isEnabled;
+    }
+
+    public void setIsEnabled(boolean isEnabled) {
+        this.isEnabled = isEnabled;
+    }
+
+    public String getCreated_by() {
+        return Created_by;
+    }
+
+    public void setCreated_by(String Created_by) {
+        this.Created_by = Created_by;
+    }
+}
